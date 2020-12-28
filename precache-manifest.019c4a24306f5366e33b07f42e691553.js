e1a82fb2396d0f5d2a996c35568ec1e778f419cc
Dev P <dpancea@gmail.com>
2020-12-28 14:44:19 -0500

    Updates

---
+++ precache-manifest.019c4a24306f5366e33b07f42e691553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "489d3c52bea4dd76cac6880818a31048",
+    "url": "/static/media/uwhoodie.489d3c52.jpeg"
+  },
+  {
+    "revision": "d05cf0001ccbc9fe26dd1500594fa42e",
+    "url": "/static/media/sidesuit.d05cf000.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bdd64281bba788c30529",
+    "url": "/static/js/main.bdd64281.chunk.js"
+  },
+  {
+    "revision": "11c3553ceb531a45f7c8",
+    "url": "/static/js/2.11c3553c.chunk.js"
+  },
+  {
+    "revision": "bdd64281bba788c30529",
+    "url": "/static/css/main.399aade1.chunk.css"
+  },
+  {
+    "revision": "01787919daf78fbcc1f1b201e49e936a",
+    "url": "/index.html"
+  }
+];
